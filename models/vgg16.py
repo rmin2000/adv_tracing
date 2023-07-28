4062df1a6a7cd1025ef9003a9e2060103d4067b8
rmin2000 <135095340+rmin2000@users.noreply.github.com>
2023-07-28 16:56:18 +0800

    Create vgg16.py

---
+++ models/vgg16.py
@@ -0,0 +1,44 @@
+import torch.nn as nn
+
+
+def VGG16Head():
+    return nn.Sequential(
+        nn.Conv2d(3, 64, kernel_size = 3, padding = 1),
+        nn.BatchNorm2d(64),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(64, 64, kernel_size = 3, padding = 1),
+        nn.BatchNorm2d(64), 
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(kernel_size=2, stride=2)
+    )
+
+class VGG16Tail(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.features = self._make_layers([128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'])
+        self.pool1d = nn.AdaptiveAvgPool1d(512)
+        self.classifier = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.pool1d(out)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 64
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+
+if __name__ == '__main__':
+    pass
