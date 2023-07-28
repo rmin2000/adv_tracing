7ad569717ebd0259137b23d0e0c22d4753074f49
rmin2000 <135095340+rmin2000@users.noreply.github.com>
2023-07-28 20:49:52 +0800

    Update trace_data_limited.py

--- trace_data_limited.py
+++ trace_data_limited.py
@@ -28,8 +28,8 @@ if __name__ == '__main__':
     means, stds = eval(f'{args.dataset_name}_means'), eval(f'{args.dataset_name}_stds')
     Head, Tail = eval(f'{args.model_name}Head'), eval(f'{args.model_name}Tail')
 
-    model_dir = f'saved_models/{args.model_name}-{args.dataset_name}'
-    adv_dir = f'saved_adv_examples/{args.model_name}-{args.dataset_name}-100heads'
+    model_dir = f'/ssddata1/user02/tracing/saved_models/{args.model_name}-{args.dataset_name}'
+    adv_dir = f'/ssddata1/user02/tracing/saved_adv_examples/{args.model_name}-{args.dataset_name}-100heads'
 
     # load the tail of the model
     normalizer = transforms.Normalize(means, stds)
@@ -47,30 +47,29 @@ if __name__ == '__main__':
         heads[-1].eval()
         watermarks.append(Watermark.load(f'{model_dir}/head_{i}/watermark.npy'))
     overall_acc = 0
-    for tmp_idx in range(10):
-        acc_list = []
-        for a in args.attacks:
-            correct = 0
-            for i in range(args.num_models):
-                adv_npz = np.load(f'{adv_dir}/head_{i}/{a}.npz')
-                Loss = nn.CrossEntropyLoss()
-                X, X_attacked, y = adv_npz['X'][:args.num_samples], adv_npz['X_attacked'][:args.num_samples], adv_npz['y'][:args.num_samples]
-                X, X_attacked, y = torch.tensor(X).to(device), torch.tensor(X_attacked).to(device), torch.tensor(y).to(device)
+    
+    for a in args.attacks:
+        correct = 0
+        for i in range(args.num_models):
+            adv_npz = np.load(f'{adv_dir}/head_{i}/{a}.npz')
+            Loss = nn.CrossEntropyLoss()
+            X, X_attacked, y = adv_npz['X'][:args.num_samples], adv_npz['X_attacked'][:args.num_samples], adv_npz['y'][:args.num_samples]
+            X, X_attacked, y = torch.tensor(X).to(device), torch.tensor(X_attacked).to(device), torch.tensor(y).to(device)
 
-                CE_loss = torch.stack([Loss(tail(head(wm(normalizer(X_attacked)))).softmax(-1), y) for wm, head in zip(watermarks, heads)], axis = 0)
-                
-                diffs_sum = torch.stack([wm.get_values(torch.abs(X - X_attacked)).sum() for wm in watermarks], axis = 0)
-                
-                score = diffs_sum + args.alpha * CE_loss
-                wrong_pred_list = []
-
-                out = torch.stack([tail(head(wm(normalizer(X_attacked)))).argmax(axis = -1) for wm, head in zip(watermarks, heads)], axis = 0)
-                wrong_pred = (out == y[None,:]).sum(-1) > 0
-                
-                score[wrong_pred] = np.inf
-                
+            CE_loss = torch.stack([Loss(tail(head(wm(normalizer(X_attacked)))).softmax(-1), y) for wm, head in zip(watermarks, heads)], axis = 0)
             
-                result = score.topk(1, largest=False)[1]
-                correct += torch.sum(result == i).item()
-            print((f'Attack {a}, tracing accuracy {correct / args.num_models}.'))
+            diffs_sum = torch.stack([wm.get_values(torch.abs(X - X_attacked)).sum() for wm in watermarks], axis = 0)
+            
+            score = diffs_sum + args.alpha * CE_loss
+            wrong_pred_list = []
+
+            out = torch.stack([tail(head(wm(normalizer(X_attacked)))).argmax(axis = -1) for wm, head in zip(watermarks, heads)], axis = 0)
+            wrong_pred = (out == y[None,:]).sum(-1) > 0
+            
+            score[wrong_pred] = np.inf
+            
+        
+            result = score.topk(1, largest=False)[1]
+            correct += torch.sum(result == i).item()
+        print((f'Attack {a}, tracing accuracy {correct / args.num_models}.'))
             
