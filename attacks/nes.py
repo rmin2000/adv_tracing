880a9a1de9d9a022d0148ddd0caf40e394928d19
rmin2000 <135095340+rmin2000@users.noreply.github.com>
2023-08-20 20:31:33 +0800

    Update nes.py

--- attacks/nes.py
+++ attacks/nes.py
@@ -1,15 +1,14 @@
 import torch
 import torch.nn as nn
-
+from torchvision import transforms
 import numpy as np
 import os
 import argparse
-
-from models import *
-from datasets import *
-from watermark import Watermark
-
 from art.estimators.classification import PyTorchClassifier
+
+from models import VGG16Head, VGG16Tail, ResNet18Head, ResNet18Tail
+import config
+from watermark import Watermark
 from attacks.score import ScoreBlackBoxAttack
 from attacks import *
 
